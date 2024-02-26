ef22ec6ffa97287c64f6deb02e6061c07f18b843
Developer B <developerB@example.com>
2024-02-26 13:50:10 +0200

    add depth two

---
+++ depthOne/depthTwo/test.js
@@ -0,0 +1,9 @@
+const devName= "developer B"
+const age= 22
+const title= "QA"
+
+for(let i=0; i<4; i++){
+    console.log("developer name: "+devName+
+    "/n and the age is: "+age+
+    "/n and the title is : "+title)
+}
