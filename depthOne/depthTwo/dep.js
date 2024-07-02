0feed06f1d466cc2232ad3d2679d53f10d34694d
wafaa-abdel-massih <wafaa.abdelmassih@titrias.com>
2024-07-02 19:03:45 +0300

    h

--- depthOne/depthTwo/dep.js
+++ depthOne/depthTwo/dep.js
@@ -5,13 +5,6 @@ import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import * as imaps from 'imap-simple';
 
-import 'bootstrap';
-import exp from 'constants';
-import { describe, it } from "mocha";
-import { defineConfig } from 'cypress';
-import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
-import * as imaps from 'imap-simple';
-
 
 describe("", ()=> {
     it("", )
