52abfd2ce7dce8f97268bf0613c5926efa4e09de
wafaa-abdel-massih <wafaa.abdelmassih@titrias.com>
2024-07-02 20:06:15 +0300

    remove dep

--- depthOne/depthTwo/test.js
+++ depthOne/depthTwo/test.js
@@ -1,11 +1,3 @@
-import 'bootstrap';
-import exp from 'constants';
-import { describe, it } from "mocha";
-import { defineConfig } from 'cypress';
-import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
-import * as imaps from 'imap-simple';
-
-
 const devName= "developer B"
 const age= 22
 const title= "QA"
