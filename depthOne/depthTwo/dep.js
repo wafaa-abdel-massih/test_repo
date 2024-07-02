0dcfb22afcd3e5fe8be3b96f94a6fa84349a5e75
wafaa-abdel-massih <wafaa.abdelmassih@titrias.com>
2024-07-02 18:58:16 +0300

    add dependencies

--- depthOne/depthTwo/dep.js
+++ depthOne/depthTwo/dep.js
@@ -1,6 +1,17 @@
 import 'bootstrap';
 import exp from 'constants';
 import { describe, it } from "mocha";
+import { defineConfig } from 'cypress';
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+import * as imaps from 'imap-simple';
+
+import 'bootstrap';
+import exp from 'constants';
+import { describe, it } from "mocha";
+import { defineConfig } from 'cypress';
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+import * as imaps from 'imap-simple';
+
 
 describe("", ()=> {
     it("", )
---
+++ secondFolder/depppp.js
@@ -0,0 +1,11 @@
+import 'bootstrap';
+import exp from 'constants';
+import { describe, it } from "mocha";
+import { defineConfig } from 'cypress';
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+import * as imaps from 'imap-simple';
+
+const test = "test"
+const test1 = "test"
+const test2 = "test"
+const test3 = "test"
