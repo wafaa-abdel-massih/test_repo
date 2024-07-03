7a63b5fb3074ebee84025e54b2054e63cd6e21db
wafaa-abdel-massih <wafaa.abdelmassih@titrias.com>
2024-07-03 12:06:41 +0300

    remove file and some dep

--- secondFolder/depppp.js
+++ secondFolder/depppp.js
@@ -1,5 +1,4 @@
 import 'bootstrap';
-import exp from 'constants';
 import { describe, it } from "mocha";
 import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
