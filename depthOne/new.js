f400ca7850968b4f3f0ff8e98e173c4f7f4558ac
well <well@gmail.com>
2024-07-16 00:27:17 +0300

    add file in depth one

---
+++ depthOne/new.js
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { ChatwootService } from '@codenteam/ui/chatwoot/chatwoot.service';
+import { environment } from '../environments/environment';
+
+const test = "test"
+const test1 = "test"
+const test2 = "test"
+const test3 = "test"
