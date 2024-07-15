0b37f9988d4b0f966950636b9f28b5f03f3ed68c
marc <marc@gmail.com>
2024-07-16 00:25:02 +0300

    add more imports

---
+++ secondFolder/higherDepth/new.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'cypress';
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+import * as imaps from 'imap-simple';
+/// <reference types="cypress" />
+import exp from 'constants';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-control-regex */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-namespace */
+import { Route } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { SettingsComponent } from './settings/settings.component';
+import { MainLayoutComponent } from './layouts/main/main.component';
